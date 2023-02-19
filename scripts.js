a6887785f91f4dafcdda66e1251ce0c028b7f559
Betiana Gavotti <122955447+BetianaGavotti@users.noreply.github.com>
2023-02-19 00:37:43 -0300

    boton mostrar y ocultar info contacto

--- scripts.js
+++ scripts.js
@@ -3,3 +3,16 @@ function imprimir(){
     window.print();
 }
 
+function ocultarMostrar(){
+    var contacto = document.querySelector('#InfoContacto');
+    var button = document.querySelector('#botonOcultar');
+    if (contacto.style.display === "" || contacto.style.display === "none") {
+        contacto.style.display = "initial";
+        button.setAttribute("value","Ocultar Datos de Contacto")
+    }
+    else{
+        contacto.style.display = "none";
+        button.setAttribute("value","Mostrar Datos de Contacto")
+    }
+}
+
